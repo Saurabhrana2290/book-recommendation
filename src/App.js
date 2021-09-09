7b9000c861cba7f314280cbc6cc230863527bdb9
Saurabhrana2290 <saurabhrana532@gmail.com>
2021-09-09 12:08:36 +0530

    Update- added event listener

--- src/App.js
+++ src/App.js
@@ -1,28 +1,36 @@
 import './App.css';
-import book from './book.jpg';
+import React,{useState} from 'react';
 function App() {
+  var [book1Name,setBook1Name] = useState("");
+  var [book2Name,setBook2Name] = useState("");
+  function clickHandlerBtn1(){
+    setBook1Name("Let us code");
+    setBook2Name("Code for me");
+  }
+  function clickHandlerBtn2(){
+    setBook1Name("Abc");
+    setBook2Name("Shakes");
+  }
+  function clickHandlerBtn3(){
+    setBook1Name("flintoff");
+    setBook2Name("Rd");
+  }
   return (
     <div className="App">
-      
-      <h1> 📚 Goodbooks!</h1>
-
+      <h1> 📚 Goodbooks!</h1> 
       <p>Checkout my favourite books. Select a genre to get started</p>
-
       <div className="subButton">
-        <button id="btn1">Javascript</button>
-        <button id="btn2">fiction</button>
-        <button id="btn3">business</button>
+        <button id="btn1" onClick={clickHandlerBtn1}>Javascript</button>
+        <button id="btn2" onClick={clickHandlerBtn2}>fiction</button>
+        <button id="btn3" onClick={clickHandlerBtn3}>business</button>
       </div>
-
       <hr />
-
       <div className="bookNames">
         <ul className="books">
-          <li id="book1" className="nonBullets"></li>
-          <li id="book2" className="nonBullets"></li>
+          <li id="book1" className="nonBullets">{book1Name}</li>
+          <li id="book2" className="nonBullets">{book2Name}</li>
         </ul>
       </div>
-
     </div>
   );
 }
